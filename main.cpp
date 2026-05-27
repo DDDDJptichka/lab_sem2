06195f19c8d44406441409e3cc56ed37fa2cf762
Z_maker <daniilmanucaran36964@gmail.com>
2026-05-28 01:46:47 +0300

    googletests + lab3

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <iostream>
+
+#include "includes/Exception.h"
+
+#include "includes/DynamicArray.h"
+#include "includes/LinkedList.h"
+#include "includes/ArraySequence.h"
+#include "includes/ListSequence.h"
+#include "includes/Sequence.h"
+#include "includes/DiagonalMatrix.h"
+
+//#include <gtest/gtest.h>
+
+int main(){
+
+    std::cout << 1;
+    return 0;
+
+}
+// rmdir /s /q build
+// cmake -S . -B build -G "MinGW Makefiles"
+// cmake --build build
